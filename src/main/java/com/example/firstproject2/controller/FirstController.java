913f6ea01e2c7815bf01f69104921bedb52b6c8b
hyeon0415 <82595578+hyeon0415@users.noreply.github.com>
2023-06-11 15:28:42 +0900

    2023. 06. 11
    first controller

---
+++ src/main/java/com/example/firstproject2/controller/FirstController.java
@@ -0,0 +1,16 @@
+package com.example.firstproject2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FirstController {
+
+    @GetMapping("/hi")
+    public String niceToMeetYou(Model model) {
+
+        model.addAttribute("username", "Hyeon");
+        return "greetings"; // templates/greetings.mustache -> 브라우저로 전송
+    }
+}
