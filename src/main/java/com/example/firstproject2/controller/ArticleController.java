94a5875d3765912a7fe2a4f9a6d64f34cc5ed83a
hyeon0415 <82595578+hyeon0415@users.noreply.github.com>
2023-06-18 13:58:42 +0900

    2023. 06. 18
    lombok, logging, refactoring

--- src/main/java/com/example/firstproject2/controller/ArticleController.java
+++ src/main/java/com/example/firstproject2/controller/ArticleController.java
@@ -3,12 +3,14 @@ package com.example.firstproject2.controller;
 import com.example.firstproject2.dto.ArticleForm;
 import com.example.firstproject2.entity.Article;
 import com.example.firstproject2.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@Slf4j // logging을 위한 annotation
 public class ArticleController {
 
     @Autowired // 스프링부트가 미리 생성해놓은 객체를 가져다가 연결
@@ -21,15 +23,18 @@ public class ArticleController {
 
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form) {
-        System.out.println(form.toString());
+        //System.out.println(form.toString()); -> logging으로 대체
+        log.info(form.toString());
 
         // 1. dto를 entity로 전환
         Article article = form.toEntity();
-        System.out.println(article.toString());
+        //System.out.println(article.toString());
+        log.info(article.toString());
 
         // 2. repository에게 entity를 DB안에 저장하게 함
         Article saved = articleRepository.save(article);
-        System.out.println(saved.toString());
+        //System.out.println(saved.toString());
+        log.info(saved.toString());
 
         return "";
     }
--- src/main/java/com/example/firstproject2/dto/ArticleForm.java
+++ src/main/java/com/example/firstproject2/dto/ArticleForm.java
@@ -1,24 +1,15 @@
 package com.example.firstproject2.dto;
 
 import com.example.firstproject2.entity.Article;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
+@AllArgsConstructor // 생성자
+@ToString
 public class ArticleForm {
 
     private String title;
     private String content;
-
-    public ArticleForm(String title, String content) {
-        this.title = title;
-        this.content = content;
-    }
-
-    @Override
-    public String toString() {
-        return "ArticleForm{" +
-                "title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 
     public Article toEntity() {
         return new Article(null, title, content);
