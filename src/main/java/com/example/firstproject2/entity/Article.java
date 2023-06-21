2df484d0e7ca13a2382e14e3a9e7556d9595f061
hyeon0415 <82595578+hyeon0415@users.noreply.github.com>
2023-06-21 22:51:11 +0900

    2023. 06. 21
    link, redirect

--- src/main/java/com/example/firstproject2/entity/Article.java
+++ src/main/java/com/example/firstproject2/entity/Article.java
@@ -1,6 +1,7 @@
 package com.example.firstproject2.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
@@ -13,6 +14,7 @@ import javax.persistence.Id;
 @AllArgsConstructor
 @NoArgsConstructor // 디폴트 생성자
 @ToString
+@Getter
 public class Article {
 
     @Id
