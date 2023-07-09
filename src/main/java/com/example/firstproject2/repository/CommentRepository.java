e9bd824fffa2f6ef7da9da6105b90f9c23b96acc
hyeon0415 <82595578+hyeon0415@users.noreply.github.com>
2023-07-09 19:02:30 +0900

    2023. 07. 09
    query , test

--- src/main/java/com/example/firstproject2/repository/CommentRepository.java
+++ src/main/java/com/example/firstproject2/repository/CommentRepository.java
@@ -3,6 +3,7 @@ package com.example.firstproject2.repository;
 import com.example.firstproject2.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -12,8 +13,19 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
                    "FROM comment " +
                    "WHERE article_id = :articleId",
                     nativeQuery = true)
-    List<Comment> findByArticleId(Long articleId);
+    List<Comment> findByArticleId(@Param("articleId") Long articleId);
 
     // 특정 닉네임의 모든 댓글 조회
-    List<Comment> findByNickName(String nickname);
+    List<Comment> findByNickname(@Param("nickname") String nickname);
 }
+
+
+
+
+
+
+
+
+
+
+
