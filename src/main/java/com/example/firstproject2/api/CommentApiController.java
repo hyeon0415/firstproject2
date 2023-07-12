3ca20c5cbb1cb35888a20a5ed0c454cf319063dc
hyeon0415 <82595578+hyeon0415@users.noreply.github.com>
2023-07-12 22:50:25 +0900

    2023. 07. 12
    cti, svc 에 Post

--- src/main/java/com/example/firstproject2/api/CommentApiController.java
+++ src/main/java/com/example/firstproject2/api/CommentApiController.java
@@ -5,17 +5,17 @@ import com.example.firstproject2.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RestController
 public class CommentApiController {
     @Autowired
     private CommentService commentService;
 
     // 댓글 목록 조회
-    @GetMapping("/api/articles/{articleId}/comments")
+    @GetMapping("api/articles/{articleId}/comments")
     public ResponseEntity<List<CommentDto>> comments(@PathVariable Long articleId) {
         // 서비스에게 위임
         List<CommentDto> dtos = commentService.comments(articleId);
@@ -25,6 +25,15 @@ public class CommentApiController {
     }
 
     // 댓글 생성
+    @PostMapping("/api/articles/{articleId}/comments")
+    public ResponseEntity<CommentDto> create(@PathVariable long articleId,
+                                             @RequestBody CommentDto dto) {
+        // 서비스에게 위임
+        CommentDto createDto = commentService.create(articleId, dto);
+
+        // 결과 응답
+        return ResponseEntity.status(HttpStatus.OK).body(createDto);
+    }
 
     // 댓글 수정
 
--- src/main/java/com/example/firstproject2/service/CommentService.java
+++ src/main/java/com/example/firstproject2/service/CommentService.java
@@ -1,6 +1,7 @@
 package com.example.firstproject2.service;
 
 import com.example.firstproject2.dto.CommentDto;
+import com.example.firstproject2.entity.Article;
 import com.example.firstproject2.entity.Comment;
 import com.example.firstproject2.repository.ArticleRepository;
 import com.example.firstproject2.repository.CommentRepository;
@@ -10,8 +11,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CommentService {
@@ -21,18 +24,37 @@ public class CommentService {
     private ArticleRepository articleRepository;
 
     public List<CommentDto> comments(Long articleId) {
-        // 조회: 댓글 목록
+        // 조회: 댓글 목록을 조회
         List<Comment> comments = commentRepository.findByArticleId(articleId);
 
-        // 변환: 엔티티 -> DTo
-        List<CommentDto> dtos = new ArrayList<CommentDto>();
-        for (int i = 0; i < comments.size(); i++) {
-            Comment c = comments.get(i);
-            CommentDto dto = CommentDto.createCommentDto(c);
-            dtos.add(dto);
-        }
+        // 변환: 엔티티 -> Dto
+//        List<CommentDto> dtos = new ArrayList<CommentDto>();
+//        for(int i = 0; i < comments.size(); i++) {
+//            Comment c = comments.get(i);
+//            CommentDto dto = CommentDto.createCommentDto(c);
+//            dtos.add(dto);
+//        }
 
-        // 반환
-        return dtos;
+        반환:
+        return commentRepository.findByArticleId(articleId)
+                .stream()
+                .map(comment -> CommentDto.createCommentDto(comment))
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public CommentDto create(long articleId, CommentDto dto) {
+        // 게시글 조회 및 예외처리
+        Article article = articleRepository.findById(articleId)
+                .orElseThrow(() -> new IllegalArgumentException("댓글 생성 실패! 대상 게시글 없음"));
+
+        // 댓글 엔티티 생성
+        Comment comment = Comment.createComment(dto, article);
+
+        // 댓글 엔티티를 DB로 저장
+        Comment created = commentRepository.save(comment);
+
+        // DTo로 변경하여 반환
+        return CommentDto.createCommentDto(created);
     }
 }
