355c63111aa82d383369165c2f5963d4ac351df1
hyeon0415 <82595578+hyeon0415@users.noreply.github.com>
2023-07-14 23:37:12 +0900

    2023. 07. 14
    comment view

--- src/main/java/com/example/firstproject2/controller/ArticleController.java
+++ src/main/java/com/example/firstproject2/controller/ArticleController.java
@@ -1,8 +1,10 @@
 package com.example.firstproject2.controller;
 
 import com.example.firstproject2.dto.ArticleForm;
+import com.example.firstproject2.dto.CommentDto;
 import com.example.firstproject2.entity.Article;
 import com.example.firstproject2.repository.ArticleRepository;
+import com.example.firstproject2.service.CommentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -22,6 +24,9 @@ public class ArticleController {
 
     @Autowired // 스프링부트가 미리 생성해놓은 객체를 가져다가 연결
     private ArticleRepository articleRepository;
+
+    @Autowired
+    private CommentService commentService;
 
     @GetMapping("/articles/new")
     public String newArticleForm() {
@@ -52,9 +57,11 @@ public class ArticleController {
 
         // 1: id로 데이터를 가져옴
         Article articleEntity = articleRepository.findById(id).orElse(null);
+        List<CommentDto> commentDtos = commentService.comments(id);
 
         // 2: 가져온 데이터를 모델에 등록
         model.addAttribute("article", articleEntity);
+        model.addAttribute("commentDtos", commentDtos);
 
         // 3: 보여줄 페이지를 설정!
         return "articles/show";
