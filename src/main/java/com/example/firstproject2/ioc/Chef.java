201c0e7b97bf26a3b64e996907f897e506d740cb
hyeon0415 <82595578+hyeon0415@users.noreply.github.com>
2023-07-27 23:18:30 +0900

    2023. 07. 27
    DI (1)

---
+++ src/main/java/com/example/firstproject2/ioc/Chef.java
@@ -0,0 +1,15 @@
+package com.example.firstproject2.ioc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class Chef {
+
+    public String cook(String menu) {
+        // 재료 준비
+        //Pork pork = new Pork("한돈 등심");
+        Beef beef = new Beef("한우 꽃등심");
+
+        // 요리 반환
+        return beef.getName() + "으로 만든 " + menu;
+    }
+}
